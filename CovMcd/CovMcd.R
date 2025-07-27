48c4428737c4730cef9c8cd002f7f4259f47f1c9
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-07-27 07:32:10 +0700

    CovMcd (rrcov) With (In) R Software

---
+++ CovMcd/CovMcd.R
@@ -0,0 +1,15 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Scalable Robust Estimators with High Breakdown Point Use CovMcd (rrcov) With (In) R Software
+install.packages("rrcov")
+library("rrcov")
+CovMcd = read.csv("https://raw.githubusercontent.com/timbulwidodostp/CovMcd/main/CovMcd/CovMcd.csv",sep = ";")
+# Estimation A Scalable Robust Estimators with High Breakdown Point Use CovMcd (rrcov) With (In) R Software
+CovMcd <- CovMcd(CovMcd)
+summary(CovMcd)
+# Scalable Robust Estimators with High Breakdown Point Use CovMcd (rrcov) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
